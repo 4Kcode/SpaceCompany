4e8206fc21894ca863a9d3dd5d865d2c48a17736
4Kcode <148824532+4Kcode@users.noreply.github.com>
2024-06-07 11:37:13 -0500

    Update Licenses.js

--- Licenses.js
+++ Licenses.js
@@ -1,1 +1,14 @@
+	const element = document.getElementsByTagName('body')[0];
+	const startDate = new Date("2024-07-1");
+	const endDate = new Date("2024-07-2");
 
+	function checkDate() {
+		const today = new Date();
+		if(today >= startDate && today <= endDate) {
+		} else {
+			element.style.display = "none";
+		}			
+	}
+
+	checkDate();
+  setInterval(checkTime,3600000);
